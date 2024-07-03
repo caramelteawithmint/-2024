af9ed8bb9d9f28f60efd8267c28195d913968421
caramelteawithmint <146834163+caramelteawithmint@users.noreply.github.com>
2024-07-03 07:03:50 +0300

    database connect

---
+++ config.py
@@ -0,0 +1,4 @@
+host = "localhost"
+user = "your_user"
+password= "YES"
+db_name = "your_db_name"
--- database_for_api.py
+++ database_for_api.py
@@ -1,1 +1,19 @@
 import mysql.connector
+import pymysql
+import pymysql.cursors
+import getpass 
+#from config import host, user, password, db_name 
+
+
+
+try:
+    connection = pymysql.connect(
+        host="localhost",
+        user=input("Имя пользователя: "),
+        password=getpass("Пароль: "),
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print ("Successfully connected")
+except Exception as ex:    
+    print("Connection refused..")
+    print(ex)
