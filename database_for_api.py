de4d24639e57a55dc66a8be43f6c8cb01705d58f
caramelteawithmint <146834163+caramelteawithmint@users.noreply.github.com>
2024-07-03 15:20:18 +0300

    table

--- database_for_api.py
+++ database_for_api.py
@@ -41,3 +41,17 @@ except Error as e:
     print("Connection refused..")
     print(e)
 
+create_vacancies_table_query = """
+CREATE TABLE vacancies(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vacancies_id_list VARCHAR(100)
+    area_list VARCHAR(100)
+    scedule_list VARCHAR(100)
+    professional_roles_list VARCHAR(100)
+    employment_list VARCHAR(100)
+    experience_list VARCHAR(100)
+) """
+
+with connection.cursor() as cursor:
+    cursor.execute(create_vacancies_table_query)
+    connection.commit()
