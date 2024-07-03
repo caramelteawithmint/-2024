25a9ea29197ab059f179c40cf4f12010adb0f562
caramelteawithmint <146834163+caramelteawithmint@users.noreply.github.com>
2024-07-03 07:50:24 +0300

    create n show

--- database_for_api.py
+++ database_for_api.py
@@ -1,4 +1,4 @@
-from mysql.connector import connect
+from mysql.connector import connect, Error
 import pymysql
 import pymysql.cursors
 import getpass 
@@ -10,11 +10,34 @@ try:
     with connect(
         host="localhost",
         user=input("Имя пользователя: "),
-        password=getpass("Пароль: "),
-        cursorclass=pymysql.cursors.DictCursor
+        password=getpass.getpass("Пароль: "),
+        #cursorclass=pymysql.cursors.DictCursor
     ) as connection:
+        create_db_query = "CREATE DATABASE head_hunter_vacancy"
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
         print ("Successfully connected")
 
 except Exception as ex:    
     print("Connection refused..")
     print(ex)
+
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Имя пользователя: "),
+        password=getpass.getpass("Пароль: "),
+        #cursorclass=pymysql.cursors.DictCursor
+    ) as connection:
+        show_db_query = "SHOW DATABASES"
+        with connection.cursor() as cursor:
+            cursor.execute(show_db_query)
+            for db in cursor:
+                print(db)
+        print ("Successfully connected")
+
+except Error as e:    
+    print("Connection refused..")
+    print(e)
+
